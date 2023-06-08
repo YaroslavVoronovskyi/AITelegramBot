2d447ee44fc58ad5515e64e95403da99c9b68b21
yaroslav <yaroslav.voronovskyi@gmail.com>
2023-06-08 11:37:10 +0300

    Fixed configuration.

--- src/main/java/com/gmail/voronovskyi/yaroslav/aitelegrambot/config/TelegramConfig.java
+++ src/main/java/com/gmail/voronovskyi/yaroslav/aitelegrambot/config/TelegramConfig.java
@@ -7,7 +7,7 @@ public class TelegramConfig {
 
     public static final String BASE_URL = "https://api.telegram.org";
     public static final String BOT_PREFIX = "bot";
-    public static final String TOKEN = "961809770:AAEsbLJ8tNzmWo-kNGMan4NH8bxo1cfvHCI";
+    public static final String TOKEN = "";
     public static final String MESSAGE_SENT = "SENT\t";
     public static final String MESSAGE_RECEIVED = "RECEIVED\t";
 }
